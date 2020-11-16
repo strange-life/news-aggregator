b6676f4a3a61679b5160abaebfa49d168f0d1379
strange-life <strange.hao@gmail.com>
2020-11-16 19:04:06 +0800

    story detail animation optimization

--- scripts/app.js
+++ scripts/app.js
@@ -22,7 +22,7 @@ APP.Main = (function () {
   var storyStart = 0;
   var count = 100;
   var main = $('main');
-  var inDetails = false;
+  var storyDetails;
   var storyLoadCount = 0;
   var localeData = {
     data: {
@@ -80,10 +80,7 @@ APP.Main = (function () {
   }
 
   function onStoryClick(details) {
-    var storyDetails = $('sd-' + details.id);
-
-    // Wait a little time then show the story details.
-    setTimeout(showStory.bind(this, details.id), 60);
+    storyDetails = $('#sd-' + details.id);
 
     // Create and append the story. A visual change...
     // perhaps that should be in a requestAnimationFrame?
@@ -117,7 +114,7 @@ APP.Main = (function () {
       storyContent = storyDetails.querySelector('.js-content');
 
       var closeButton = storyDetails.querySelector('.js-close');
-      closeButton.addEventListener('click', hideStory.bind(this, details.id));
+      closeButton.addEventListener('click', hideStory);
 
       var headerHeight = storyHeader.getBoundingClientRect().height;
       storyContent.style.paddingTop = headerHeight + 'px';
@@ -145,83 +142,24 @@ APP.Main = (function () {
         });
       }
     }
+
+    showStory();
   }
 
-  function showStory(id) {
-    if (inDetails) return;
-
-    inDetails = true;
-
-    var storyDetails = $('#sd-' + id);
-    var left = null;
-
+  function showStory() {
     if (!storyDetails) return;
 
-    document.body.classList.add('details-active');
-    storyDetails.style.opacity = 1;
-
-    function animate() {
-      // Find out where it currently is.
-      var storyDetailsPosition = storyDetails.getBoundingClientRect();
-
-      // Set the left value if we don't have one already.
-      if (left === null) left = storyDetailsPosition.left;
-
-      // Now figure out where it needs to go.
-      left += (0 - storyDetailsPosition.left) * 0.1;
-
-      // Set up the next bit of the animation if there is more to do.
-      if (Math.abs(left) > 0.5) setTimeout(animate, 4);
-      else left = 0;
-
-      // And update the styles. Wait, is this a read-write cycle?
-      // I hope I don't trigger a forced synchronous layout!
-      storyDetails.style.left = left + 'px';
-    }
-
-    // We want slick, right, so let's do a setTimeout
-    // every few milliseconds. That's going to keep
-    // it all tight. Or maybe we're doing visual changes
-    // and they should be in a requestAnimationFrame
-    setTimeout(animate, 4);
+    requestAnimationFrame(function () {
+      storyDetails.classList.add('active');
+    });
   }
 
-  function hideStory(id) {
-    if (!inDetails) return;
+  function hideStory() {
+    if (!storyDetails) return;
 
-    var storyDetails = $('#sd-' + id);
-    var left = 0;
-
-    document.body.classList.remove('details-active');
-    storyDetails.style.opacity = 0;
-
-    function animate() {
-      // Find out where it currently is.
-      var mainPosition = main.getBoundingClientRect();
-      var storyDetailsPosition = storyDetails.getBoundingClientRect();
-      var target = mainPosition.width + 100;
-
-      // Now figure out where it needs to go.
-      left += (target - storyDetailsPosition.left) * 0.1;
-
-      // Set up the next bit of the animation if there is more to do.
-      if (Math.abs(left - target) > 0.5) {
-        setTimeout(animate, 4);
-      } else {
-        left = target;
-        inDetails = false;
-      }
-
-      // And update the styles. Wait, is this a read-write cycle?
-      // I hope I don't trigger a forced synchronous layout!
-      storyDetails.style.left = left + 'px';
-    }
-
-    // We want slick, right, so let's do a setTimeout
-    // every few milliseconds. That's going to keep
-    // it all tight. Or maybe we're doing visual changes
-    // and they should be in a requestAnimationFrame
-    setTimeout(animate, 4);
+    requestAnimationFrame(function () {
+      storyDetails.classList.remove('active');
+    });
   }
 
   // TODO
